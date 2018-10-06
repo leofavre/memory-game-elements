e436babb233eaf3a3f092a0a8e5fd13baa874780
leofavre <contato@leofavre.com>
2018-10-06 09:46:23 -0300

    refactor: Simplify selectors

--- packages/memory-game/src/state/memoryGameSelectors.js
+++ packages/memory-game/src/state/memoryGameSelectors.js
@@ -1,25 +1,18 @@
-export const getNumberOfCards = state => state.cards.length * 2;
+export const getNumberOfCards = ({ cards }) => cards.length * 2;
 
-export const getCardAtPosition = (position, state) =>
-  state.cards.find(card => card.positions.includes(position));
+export const getCardAtPosition = (position, { cards }) =>
+  cards.find(card => card.positions.includes(position));
 
-export const isCardVisible = (position, state) => {
-  const { revealed, matched } = state;
-  return revealed.includes(position) || matched.includes(position);
-};
+export const isCardRevealed = (position, { revealed }) =>
+  revealed.includes(position);
 
-export const isCardRevealed = (position, state) => {
-  const { revealed } = state;
-  return revealed.includes(position);
-};
+export const isCardMatched = (position, { matched }) =>
+  matched.includes(position);
 
-export const isCardMatched = (position, state) => {
-  const { matched } = state;
-  return matched.includes(position);
-};
+export const isCardInTheGame = (position, { revealed, matched }) =>
+  revealed.includes(position) || matched.includes(position);
 
-export const isPairOfCardsVisible = state =>
-  state.revealed.length === 2;
+export const isPairOfCardsVisible = ({ revealed }) => revealed.length === 2;
 
 export const isGameOver = (state) =>
   getNumberOfCards(state) === state.matched.length;
