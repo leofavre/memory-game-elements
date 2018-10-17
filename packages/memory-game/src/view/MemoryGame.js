dde46f89aa37958c634367264f95dd6e4cc398d3
leofavre <contato@leofavre.com>
2018-10-16 23:50:23 -0300

    feat: Enhance native `HTMLElement` with `withElmnt`

--- packages/memory-game/src/view/MemoryGame.js
+++ packages/memory-game/src/view/MemoryGame.js
@@ -1,82 +1,41 @@
-import withObservedProperties from 'observed-properties';
-import { render } from 'lit-html';
+import { withElmnt, withDomEvents, withReducer } from '@leofavre/elmnt';
 import memoryGameReducer from '../state/memoryGameReducer.js';
 import { distributeCards, requestPlayAsync } from '../state/memoryGameActions.js';
 import MemoryGameView from './MemoryGameView.js';
-import isFunction from '../helpers/isFunction.js';
-import isPromise from '../helpers/isPromise.js';
 
-export default class extends withObservedProperties(HTMLElement) {
+export default class extends withReducer(memoryGameReducer)(
+  withDomEvents(withElmnt(HTMLElement))) {
   constructor () {
     super();
-    this.attachShadow({ mode: 'open' });
-    this.state = memoryGameReducer();
     this.handleClick = this.handleClick.bind(this);
   }
 
-  static get observedProperties () {
-    return ['state', 'cards'];
-  }
-
-  propertyChangedCallback (propName, oldValue, newValue) {
-    if (oldValue !== newValue) {
-      switch (propName) {
-        case 'state':
-          if (oldValue && oldValue.events !== newValue.events) {
-            const { name, detail } = [...newValue.events].pop();
-            this.dispatchEventAndMethod(name, detail);
-          }
-          this.render();
-          break;
-
-        case 'cards':
-          this.updateState(distributeCards(newValue));
-          this.dispatchEventAndMethod('start');
-          break;
+  static get parameters () {
+    return [{
+      prop: 'state',
+      onPropChanged (newValue, oldValue) {
+        if (oldValue && oldValue.events !== newValue.events) {
+          const { name, detail } = [...newValue.events].pop();
+          this.dispatchEventAndMethod(name, detail);
+        }
+        this.updateLayout();
       }
-    }
+    }, {
+      prop: 'cards',
+      onPropChanged (newValue) {
+        this.dispatchAction(distributeCards(newValue));
+        this.dispatchEventAndMethod('start');
+      }
+    }];
   }
 
   handleClick (position) {
     return () => {
-      this.updateState(requestPlayAsync(position));
+      this.dispatchAction(requestPlayAsync(position));
     };
   }
 
-  updateState (action) {
-    let resolvedAction = action;
-
-    if (isFunction(resolvedAction)) {
-      resolvedAction = action(this.updateState.bind(this), this.state);
-    }
-
-    if (isPromise(resolvedAction)) {
-      return resolvedAction.then(asyncAction => {
-        this.state = memoryGameReducer(this.state, asyncAction);
-        return this.state;
-      });
-    } else {
-      this.state = memoryGameReducer(this.state, resolvedAction);
-      return this.state;
-    }
-  }
-
-  dispatchEventAndMethod (name, detail) {
-    const event = new CustomEvent(name, {
-      bubbles: true,
-      detail
-    });
-
-    const method = this[`on${name}`];
-
-    this.dispatchEvent(event);
-
-    if (isFunction(method)) {
-      method(event);
-    }
-  }
-
   render () {
-    render(MemoryGameView(this), this.shadowRoot);
+    return MemoryGameView(this);
   }
 }
